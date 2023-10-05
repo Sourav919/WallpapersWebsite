2d9af2157e34baa7d9b17a6777c9da5157e7e269
Sourav <94462441+Sourav919@users.noreply.github.com>
2023-10-05 23:53:15 +0530

    script.js

--- script.js
+++ script.js
@@ -1,29 +1,10 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const downloadButtons = document.querySelectorAll(".download-btn");
+
     let video = document.querySelector("video");
 
     // Pause the video by default
     video.pause();
 
-    downloadButtons.forEach(button => {
-        button.addEventListener("click", async function (event) {
-            event.preventDefault(); // Prevent default link behavior
-            
-            const imgUrl = this.getAttribute("data-img-url");
-            const fileName = getFileNameFromURL(imgUrl);
-
-            const response = await fetch(imgUrl);
-            const blob = await response.blob();
-
-            // Create a temporary anchor element to trigger the download
-            const downloadLink = document.createElement("a");
-            downloadLink.href = URL.createObjectURL(blob);
-            downloadLink.download = fileName;
-            downloadLink.click();
-            URL.revokeObjectURL(downloadLink.href);
-        });
-    });
-
+    
     // Detect scroll event
     window.addEventListener("scroll", function () {
         // Play the video when the user has scrolled down by a certain amount
@@ -38,4 +19,4 @@ document.addEventListener("DOMContentLoaded", function () {
         const parts = url.split("/");
         return parts[parts.length - 1];
     }
-});
+
