fc8bf94b52fc924a7ede1a9d3dcc4d17be5ba240
Sourav <94462441+Sourav919@users.noreply.github.com>
2023-10-06 10:27:23 +0530

    script.js

--- script.js
+++ script.js
@@ -1,22 +1,15 @@
 
     let video = document.querySelector("video");
-
-    // Pause the video by default
     video.pause();
-
-    
-    // Detect scroll event
     window.addEventListener("scroll", function () {
-        // Play the video when the user has scrolled down by a certain amount
-        if (window.scrollY > 100) { // Adjust the value as needed
+        if (window.scrollY > 100) 
+        {
             video.play();
-        } else {
+        }
+        else
+        {
             video.pause();
         }
     });
 
-    function getFileNameFromURL(url) {
-        const parts = url.split("/");
-        return parts[parts.length - 1];
-    }
 
